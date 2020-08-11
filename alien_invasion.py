9797e019e3f16fdd968858f3617340e22df8cc8f
jiayu520 <jiayu520000@outtlook.com>
2020-08-11 16:16:48 +0800

    alien

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,9 +1,11 @@
 import sys
 import pygame
 
+from alien import Alien
 from settings import Settings
 from ship import Ship
 import game_functions as gf
+from pygame.sprite import Group
 
 def run_game():
     '''初始化游戏并创建一个屏幕对象'''
@@ -17,6 +19,16 @@ def run_game():
     pygame.display.set_caption("Alien Invasion")
     #创建一艘飞船
     ship = Ship(ai_settings,screen)
+    #创建一个用于存储子弹的编组
+    bullets = Group()
+    # #创建一个外星人
+    # alien = Alien(ai_settings,screen)
+    #创建一个外星人编组
+    aliens =Group()
+    #创建外星人群
+    gf.create_fleet(ai_settings,screen,ship,aliens)
+
+
 
 
     #开始游戏的主循环
@@ -25,13 +37,22 @@ def run_game():
         # for event in pygame.event.get():
         #     if event.type == pygame.QUIT:
         #         sys.exit()
-        gf.check_events(ship)
+        gf.check_events(ai_settings,screen,ship,bullets)
             #每次循环是重绘屏幕
             # screen.fill(bg_color)
         # screen.fill(ai_settings.bg_color)
         # ship.blitme()
         ship.update()
-        gf.update_screen(ai_settings,screen,ship)
+        # bullets.update()
+        #
+        # #删除已消失的子弹
+        # for bullet in bullets.copy():
+        #     if bullet.rect.bottom <= 0:
+        #         bullets.remove(bullet)
+        gf.update_bullets(aliens,bullets)
+        gf.update_aliens(ai_settings,aliens)
+        gf.update_screen(ai_settings,screen,ship,aliens,bullets)
+
 
         #让最近的屏幕绘制可见
         pygame.display.flip()
